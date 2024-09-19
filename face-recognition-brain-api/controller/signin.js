d2ea58edd52adb0d130a4ee438d522056dec035d
Averei <raveen.edward087@gmail.com>
2024-09-19 11:36:57 +0800

    Signin error update

--- face-recognition-brain-api/controller/signin.js
+++ face-recognition-brain-api/controller/signin.js
@@ -1,25 +1,36 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json('incorrect form submission');                                     
-      }
-    db.select('email', 'hash').from('login')
-      .where('email', '=', email)
-      .then(data => {
-        const isValid = bcrypt.compareSync(password, data[0].hash);  
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      if (data.length) {
+        console.log("Stored Hash: ", data[0].hash);
+        console.log("Password Submitted: ", password);
+
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        console.log("Password Match: ", isValid);
+        
         if (isValid) {
-          return db.select('*').from('user')
+          return db.select('*').from('users')
             .where('email', '=', email)
             .then(user => {
-              res.json(user[0]);  
+              res.json(user[0]);
             })
             .catch(err => res.status(400).json('unable to get user'));
         } else {
-          res.status(400).json('wrong credentials');  
+          res.status(400).json('wrong password');  // Invalid password
         }
-      })
-      .catch(err => res.status(400).json('wrong credentials'));  
-  }
+      } else {
+        res.status(400).json('email not found');  // Email not found
+      }
+    })
+    .catch(err => res.status(400).json('wrong db query'));  // DB query error
+};
+
 
   module.exports = {
     handleSignin : handleSignin
