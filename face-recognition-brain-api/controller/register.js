929551a42fb420a733ecc4c1b9c385046a5a19f8
Averei <raveen.edward087@gmail.com>
2024-09-19 10:39:40 +0800

    updated register.js

--- face-recognition-brain-api/controller/register.js
+++ face-recognition-brain-api/controller/register.js
@@ -1,31 +1,34 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-      return res.status(400).json('incorrect form submission');                                     
-    }
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('user').returning('*').insert({
-          email: loginEmail[0].email,
-          name: name,
-          joined: new Date()
-        }).then (user =>{
-          res.json(user[0]);
-        })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json("unable to register."))
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
   }
-
-  module.exports = {
-    handleRegister
-  };
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users').returning('*').insert({
+        email: loginEmail[0],
+        name: name,
+        joined: new Date()
+      }).then(user => {
+        res.json(user[0]);
+      })
+      .catch(err => {
+        console.error('Error inserting into users:', err);
+        res.status(400).json('unable to register user');
+      });
+    })
+    .then(trx.commit)
+    .catch(trx.rollback);
+  })
+  .catch(err => {
+    console.error('Transaction error:', err);
+    res.status(400).json('unable to register');
+  });
+};
