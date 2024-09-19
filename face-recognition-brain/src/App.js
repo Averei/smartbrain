5e472e788a00b0d9e54e406cae1863bc6adb12ac
Averei <raveen.edward087@gmail.com>
2024-09-19 14:10:10 +0800

    debugged obButtonSubmit for clarifai

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -43,19 +43,23 @@ class App extends Component {
     });
   };
 
-  calculateFaceLocation = data => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputimage");
+  calculateFaceLocation = (data) => {
+    if (!data || !data.outputs || !data.outputs[0].data || !data.outputs[0].data.regions) {
+      console.error("No face data found in the API response.");
+      return {};
+    }
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     };
   };
+  
 
   displayFaceBox = box => {
     this.setState({ box: box });
@@ -67,32 +71,32 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-      fetch("https://smartbrains-la3q.onrender.com/imageurl", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          input: this.state.input
-        })
+    fetch("https://smartbrains-la3q.onrender.com/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input
       })
-      .then(response => response.json())
-      .then(response => {
-        console.log("hi", response);
-        if (response) {
-          fetch("https://smartbrains-la3q.onrender.com/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })  
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log("Clarifai API Response:", response); // Add this line to log the response
+      if (response) {
+        fetch("https://smartbrains-la3q.onrender.com/image", {
+          method: "put",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            id: this.state.user.id
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-      })
-      .catch(err => console.log(err));
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count }));
+        });
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response)); // The error is happening here
+    })
+    .catch(err => console.log(err));
   };
 
   onRouteChange = route => {
